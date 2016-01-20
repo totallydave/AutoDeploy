5c1cea8cc83e015ca9d3622ff03e5456d052ea6b
kingd <dave@totallycommunications.com>
2016-01-20 12:02:24 +0000

    removed class map autoloader from Module

--- Module.php
+++ Module.php
@@ -32,9 +32,6 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface
     public function getAutoloaderConfig()
     {
         return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-            ),
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
