824c66ad0866a09b399cd497be3d7c993d652d18
kingd <dave@totallycommunications.com>
2016-01-20 11:58:40 +0000

    added class map to composer.json and getAutoLoaderConfig

--- Module.php
+++ Module.php
@@ -32,6 +32,9 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface
     public function getAutoloaderConfig()
     {
         return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
